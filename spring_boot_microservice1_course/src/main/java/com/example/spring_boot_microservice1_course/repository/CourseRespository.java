92fd44a806c9716bf8152d1c5e6bcff483ddf32f
akshitchadha <achadha1995@gmail.com>
2023-03-04 14:54:25 +0530

    Delete spring_boot_microservice1_course directory

--- spring_boot_microservice1_course/src/main/java/com/example/spring_boot_microservice1_course/repository/CourseRespository.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_boot_microservice1_course.repository;
-
-import com.example.spring_boot_microservice1_course.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRespository extends JpaRepository<Course, Long> {
-}
